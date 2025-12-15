1acfce2dc93826043e5bffb6a930871d06481204
siobal <sblmnrd@gmail.com>
2025-12-15 22:20:50 +0800

    feat(faqs): implement FAQs accordion section
    - Install accordion from Shadcn/ui
    - Add FAQs static data

---
+++ src/components/faqs/faqs.tsx
@@ -0,0 +1,25 @@
+import TripleStar from "../ui/triple-star";
+import AccordionList from "./accordion-list";
+
+export default function FAQs() {
+  return (
+    <section className="wrapper">
+      <TripleStar />
+      <div className="flex flex-col gap-y-7.5">
+        <div className="flex items-end justify-between gap-x-20">
+          <div className="flex flex-col gap-y-1.5">
+            <h1 className="text-sub-title font-semibold">
+              Frequently Asked Questions
+            </h1>
+            <p className="text-body text-sub-foreground font-medium">
+              Read the success stories and heartfelt testimonials from our
+              valued clients. Discover why they chose Estatein for their real
+              estate needs.
+            </p>
+          </div>
+        </div>
+        <AccordionList />
+      </div>
+    </section>
+  );
+}
