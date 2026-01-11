955ddb991ffd68444e5ae4b05742d3e850709fd7
siobal <sblmnrd@gmail.com>
2026-01-11 23:33:15 +0800

    feat: add fully responsive hero section

---
+++ src/app/(main)/properties/_components/hero-section.tsx
@@ -0,0 +1,19 @@
+export default function PropertiesHero() {
+  return (
+    // Main hero container with gradient background styling
+    <section className="from-border/70 border-border scroll-mt-32.5 border-b bg-linear-to-br to-30%">
+      {/* Header: Title and description */}
+      <div className="wrapper space-y-2.5 py-12.5 lg:py-25 xl:space-y-3.5 xl:pt-37.5 xl:pb-25">
+        <h1 className="text-sub-title font-semibold">
+          Find Your Dream Property
+        </h1>
+        <p className="text-body text-sub-foreground max-w-6xl font-medium">
+          Welcome to Estatein, where your dream property awaits in every corner
+          of our beautiful world. Explore our curated selection of properties,
+          each offering a unique story and a chance to redefine your life. With
+          categories to suit every dreamer, your journey
+        </p>
+      </div>
+    </section>
+  );
+}
